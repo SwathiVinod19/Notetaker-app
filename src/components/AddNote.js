fe2c5decda56cc6358bf04880f10200dfa9cf253
SwathiVinod19 <swathi.vinod@gmail.com>
2023-09-04 12:13:28 -0400

    added code

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -4,7 +4,9 @@ const AddNote = ({ handleAddNote }) => {
     const [noteText, setNoteText] = useState('');
     const characterLimit = 200;
     const handleChange = (event) => {
+        if(characterLimit - event.target.value.length >=0){
         setNoteText(event.target.value);
+        }
     };
     const handleSaveClick = () => {
         if(noteText.trim().length > 0){
